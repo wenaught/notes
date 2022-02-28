2f047cb1bb127810466cb23205530f3764706ecc
Petr Liakhavets <petr_liakhavets@epam.com>
2022-02-28 17:22:27 +0300

    added CORS support, config file to notes.create_app
    Signed-off-by: Petr Liakhavets <petr_liakhavets@epam.com>

--- notes/__init__.py
+++ notes/__init__.py
@@ -1,7 +1,9 @@
 """Notes is a note-taking API."""
 import os
 
+import yaml
 from apiflask import APIFlask
+from flask_cors import CORS
 from flask_pymongo import PyMongo
 
 from notes.models import Note, instance
@@ -9,14 +11,16 @@ from notes.models import Note, instance
 mongo: PyMongo = PyMongo()
 
 
-def create_app() -> APIFlask:
+def create_app(config_file_name: str) -> APIFlask:
     """Create an application instance.
     Requires an environment variable MONGO_URI to be set.
 
+    :param config_file_name: name of the configuration file in Flask's instance folder.
     :return: a Flask app.
     """
-    app = APIFlask(__name__, instance_relative_config=True, title='Notes API')
-    app.config['DESCRIPTION'] = __doc__
+    app = APIFlask(__name__, instance_relative_config=True, title='Notes API', version='development')
+    app.config.from_file(config_file_name, yaml.safe_load)
+    CORS(app)
     mongo_uri = os.getenv("MONGO_URI")
     assert mongo_uri, "MONGO_URI was not set!"
     app.config['MONGO_URI'] = mongo_uri
