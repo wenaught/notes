f69d49004fae2d58f8fcdb1d46075bff5269b07e
Petr Liakhavets <petr_liakhavets@epam.com>
2022-03-24 14:50:05 +0300

    Refactor testing
    Did a major overhaul of tests according to recent changes.
    
    Signed-off-by: Petr Liakhavets <petr_liakhavets@epam.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,55 +1,54 @@
 import os
 
-import flask.testing
 import pytest
-import _pytest.fixtures, _pytest.monkeypatch
 import yaml
+from marshmallow import Schema
+from marshmallow.fields import String, Dict, List
 from mongomock.mongo_client import MongoClient
 
 import notes
-from notes.note.models import Note
+from notes.note.model import OutNoteSchema
+from notes.user.model import OutUserSchema, User, CreateUserSchema
 
-data_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data/notes.yaml")
-test_data = yaml.safe_load(open(data_file_path, 'r'))
-endpoint = '/note'
+schemas = {
+    'user_out': OutUserSchema(),
+    'note_out': OutNoteSchema(),
+    'many_note_out': OutNoteSchema(many=True),
+    'verification_failed': Schema.from_dict({"message": String(),
+                                             "detail": Dict(keys=String,
+                                                            values=Dict(keys=String,
+                                                                        values=List(String)))})(),
+    'http_error': Schema.from_dict({"message": String(),
+                                    "detail": Dict()})()
+}
 
 
 class PyMongoMock(MongoClient):
     def init_app(self, app):
         return super().__init__()
 
+data_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_data.yml")
+test_data = yaml.safe_load(open(data_file_path, "r"))
 
 @pytest.fixture()
-def app(monkeypatch: _pytest.monkeypatch) -> flask.Flask:
-    """Create a Flask app object instance. Drops the MongoDB database used for testing.
-
-    :param monkeypatch: a pytest monkeypatch fixture.
-    :return: the app instance.
-    """
-    if os.getenv('MOCK_MONGO'):
-        monkeypatch.setattr(notes, 'mongo', PyMongoMock())
-    yield notes.create_app('test.yaml')
-    if not os.getenv('MOCK_MONGO'):
-        notes.mongo.cx.drop_database(os.environ["MONGO_URI"].split('/')[-1])
+def app(monkeypatch):
+    if os.getenv("MOCK_MONGO"):
+        monkeypatch.setattr(notes, "mongo", PyMongoMock())
+    yield notes.create_app("test.yaml")
+    if not os.getenv("MOCK_MONGO"):
+        notes.mongo.cx.drop_database(os.environ["MONGO_URI"].split("/")[-1])
 
 @pytest.fixture()
-def fill_notes(request: _pytest.fixtures.SubRequest, app: flask.Flask) -> list[dict]:
-    """Fill database with Note documents.
-
-    :param request: pytest Request object instance.
-    :param app: Flask application object instance.
-    :return: list of dicts corresponding to created Note documents.
-    """
-    contents = request.param['contents']
-    for note in contents:
-        Note(**note).commit()
-    return contents
-
-@pytest.fixture()
-def client(app: flask.Flask) -> flask.testing.FlaskClient:
-    """Create a Flask testing client.
-
-    :param app: Flask application object instance.
-    :return: the testing client.
-    """
+def client(app):
     return app.test_client()
+
+
+@pytest.fixture()
+def default_user(app, request):
+    if not request.param:
+        return
+    user_data = test_data["users"]["user_valid_all"]
+    with app.app_context():
+        user = User(**CreateUserSchema().load(user_data))
+        user.commit()
+        return OutUserSchema().dump(user)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,64 +1,32 @@
+import logging
+
 import pytest
+from marshmallow import ValidationError
 
-from .conftest import test_data, endpoint
+from conftest import test_data, schemas
 
+logger = logging.getLogger(__name__)
 
-def test_get_docs(client):
-    response = client.get('/docs')
-    assert response.status_code == 200, f"/docs endpoint responded with unexpected code: {response.status_code}."
-    assert "text/html" in response.content_type,\
-        f"/docs endpoint responded with invalid content type: {response.content_type}."
-
-
-@pytest.mark.parametrize("fill_notes", test_data['get'], indirect=True, ids=[data['id'] for data in test_data['get']])
-def test_get_notes(fill_notes, client):
-    for note in fill_notes:
-        response = client.get(f"{endpoint}/{note['title']}")
-        assert response.status_code == 200
-        assert isinstance(response.json, dict)
-        assert note == response.json
-
-
-@pytest.mark.parametrize("fill_notes", test_data['get_all'], indirect=True, ids=[data['id'] for data in test_data['get_all']])
-def test_get_all_notes(fill_notes, client):
-    response = client.get(endpoint)
-    assert response.status_code == 200
-    assert isinstance(response.json, list)
-    assert fill_notes == response.json
-
-
-@pytest.mark.parametrize("post_test_data", test_data['post'], ids=[data['id'] for data in test_data['post']])
-def test_post_note(client, post_test_data) -> None:
-    for content in post_test_data['contents']:
-        response = client.post(endpoint, json=content)
-        assert response.json.get("title") == content["title"], \
-            f"POST response didn't contain expected 'title' value: {response.json}"
-        assert response.json.get("text") == content["text"], \
-            f"POST response didn't contain expected 'text' value: {response.json}"
-        assert all([
-            "added_timestamp" in response.json,
-            "modified_timestamp" in response.json,
-            response.json.get("added_timestamp") == response.json.get("modified_timestamp")
-        ]), f"POST response didn't contain expected timestamps: {response.json}"
-
-
-@pytest.mark.parametrize("get_test_data", test_data['get_posted'], ids=[data['id'] for data in test_data['get_posted']])
-def test_get_posted_notes(client, get_test_data) -> None:
-    for content in get_test_data['contents']:
-        post_response = client.post(endpoint, json=content)
-        get_response = client.get(f'{endpoint}/{content["title"]}')
-        assert get_response.json == post_response.json, \
-            f"GET response didn't match POST response: {get_response.json}, {post_response.json}"
-
-
-@pytest.mark.parametrize("get_all_test_data", test_data['get_all_posted'], ids=[data['id'] for data in test_data['get_all_posted']])
-def test_get_all_posted_notes(client, get_all_test_data):
-    expected_list = []
-    for content in get_all_test_data['contents']:
-        response = client.post(endpoint, json=content)
-        assert response.status_code == 201, f"POST request to '/notes' with body {content} was unsuccessful."
-        expected_list.append(response.json)
-    actual_list = client.get(endpoint).json
-    assert sorted(expected_list, key=lambda item: item['title']) == \
-           sorted(actual_list, key=lambda item: item['title']), \
-        f"GET response to get all notes didn't match expected response: {expected_list}, {actual_list}"
+@pytest.mark.parametrize("test_case,default_user",
+                         zip(test_data["test_cases"], [case.get('default_user') for case in test_data["test_cases"]]),
+                         ids=[case["id"] for case in test_data["test_cases"]],
+                         indirect=["default_user"])
+def test_app(client, test_case, default_user):
+    headers = {}
+    if default_user:
+        headers["Authorization"] = f"Bearer {default_user['token']}"
+    for request in test_case['requests']:
+        logger.info(request['method'] + ' request to ' + request['endpoint'])
+        response = getattr(client, request['method'])(request['endpoint'],
+                                                      json=request.get('data'),
+                                                      query_string=request.get('query'),
+                                                      headers=headers)
+        assert response.status_code == request['code'], "Unexpected status code"
+        if request.get('response_schema'):
+            try:
+                schemas[request['response_schema']].load(response.json)
+            except ValidationError:
+                raise AssertionError(f"Response didn't conform to expected schema. Response: {response.data}")
+        if request.get('keep_token'):
+            logger.info('saved token for further requests')
+            headers["Authorization"] = f"Bearer {response.json['token']}"
